35d1d9df7c964d046710b0803a24b5a6c74238c4
Wycliffe Ndiba <w.ndiba@gmail.com>
2022-06-14 22:37:37 +0300

    Done.

--- src/index.js
+++ src/index.js
@@ -1,56 +1,26 @@
-// document.addEventListener("DOMContentLoaded", () => {
-//   let form = document.querySelector('form')
-//   form.addEventListener('submit', (e) => {
-//     e.preventDefault()
-//     buildToDo(e.target.new_todo.value)
-//     form.reset()
-//   })
-// });
-
-// function buildToDo(todo) {
-//   let p = document.createElement('p')
-//   let btn = document.createElement('button')
-//   btn.addEventListener('click', handleDelete)
-//   btn.textContent = 'x'
-//   p.textContent = `${todo} `
-//   p.appendChild(btn)
-//   console.log(p)
-//   document.querySelector('#list').appendChild(p)
-// }
-
-// function handleDelete(e) {
-//   e.target.parentNode.remove()
-// }
-
-// A delete function that will remove tasks from your list
-// A priority value selected from a dropdown (Links to an external site.) that is used to determine the color of the text in the list (e.g. red for high priority, yellow for medium, green for low)
-// As an additional challenge, implement a sorting functionality that displays the tasks in ascending or descending order based on priority
-// An additional input field (e.g. user, duration, date due)
-// Ability to edit tasks
-
-document.addEventListener("DOMContentLoaded", () => {
-  document.querySelector('form').addEventListener('submit', (e) => {
-    e.preventDefault()
-    tasks(e.target.value)
-    document.querySelector('form').reset()
+document.addEventListener("DOMContentLoaded", () => { // add a listener to the DOM
+  document.querySelector('form').addEventListener('submit', (e) => { // add a listener to the form
+    e.preventDefault() // prevent the form from submitting
+    tasks(e.target.value) // call the tasks function
+    document.querySelector('form').reset() // reset the form
   }
   )
 }
 );
 
 
-function tasks(task) {
-  const input = document.getElementById("new-task-description")
-  let li = document.createElement('li')
-  let btn = document.createElement('button')
-  btn.addEventListener('click', handleDelete)
-  btn.textContent = 'X'
-  li.textContent = `${input.value}`
-  li.appendChild(btn)
-  document.querySelector('#tasks').appendChild(li)
+function tasks(task) { // function to add tasks
+  const input = document.getElementById("new-task-description") // get the input
+  let li = document.createElement('li') // create a new li
+  let btn = document.createElement('button') // create a new button
+  btn.addEventListener('click', handleDelete) // add a listener to the button
+  btn.textContent = 'X' // set the text of the button
+  li.textContent = `${input.value}` // set the text of the li
+  li.appendChild(btn) // append the button to the li
+  document.querySelector('#tasks').appendChild(li) // append the li to the ul
 }
 
 
-function handleDelete(e) {
-  e.target.parentNode.remove()
+function handleDelete(e) { // function to handle the delete button
+  e.target.parentNode.remove() // remove the li
 }
